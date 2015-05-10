c0941e602ab898c1474d62a20aecd9fbeae6a966
Jonnathan Romero <jromero0621@gmail.com>
2015-05-10 16:38:04 -0500

    Actualizacion inicio carpeta

--- inicio/views.py
+++ inicio/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 class InicioView(TemplateView):
-	template_name = "index.html"
+	template_name = "inicio/index.html"
